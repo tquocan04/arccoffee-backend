4c2828e412ad243f790647c363f0f975fa130e64
Ấn Trần <tquocan04@gmail.com>
2025-05-27 00:40:32 +0700

    feat(voucher): get public voucher list

--- ArcCoffee-backend/Controllers/VoucherController.cs
+++ ArcCoffee-backend/Controllers/VoucherController.cs
@@ -3,6 +3,7 @@ using DTOs.Responses;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
+using Services;
 
 namespace ArcCoffee_backend.Controllers
 {
@@ -13,6 +14,18 @@ namespace ArcCoffee_backend.Controllers
         [HttpGet]
         [Authorize(Policy = "All")]
         public async Task<IActionResult> GetAllVouchers()
+        {
+            var result = await service.GetVoucherListAsync();
+
+            return Ok(new Response<IEnumerable<VoucherDTO>>
+            {
+                Message = "Successful.",
+                Data = result
+            });
+        }
+
+        [HttpGet("public")]
+        public async Task<IActionResult> GetAllVouchersPublic()
         {
             var result = await service.GetVoucherListAsync();
 
