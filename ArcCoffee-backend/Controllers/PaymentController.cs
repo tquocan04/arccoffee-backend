b65eb8fedb1f206b5088dba9964b0cca2308ed3a
Ấn Trần <tquocan04@gmail.com>
2025-05-30 00:54:46 +0700

    docs(api/payments): API documentation

--- ArcCoffee-backend/Controllers/PaymentController.cs
+++ ArcCoffee-backend/Controllers/PaymentController.cs
@@ -9,6 +9,12 @@ namespace ArcCoffee_backend.Controllers
     [ApiController]
     public class PaymentController(IPaymentService paymentService) : ControllerBase
     {
+        /// <summary>
+        /// LẤY DANH SÁCH PHƯƠNG THỨC THANH TOÁN.
+        /// </summary>
+        /// <response code="200">Thành công.</response>
+        /// <response code="404">Không có kết quả.</response>
+        /// <response code="500">Đã có lỗi trong quá trình thực hiện.</response>
         [HttpGet]
         public async Task<IActionResult> GetAllPayments()
         {
